a124349923b842bb2da6cec3b431dd8c9d10623c
somalingck <136596008+somalingck@users.noreply.github.com>
2023-08-12 19:40:42 +0530

    Initial commit

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -10,16 +10,30 @@ public class Car extends Vehicle {
     private int seats;
 
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
-        //Hint: Car extends Vehicle
+        super(name);
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManual = isManual;
+        this.type = type;
+        this.seats = seats;
+        this.currentGear = 1;
     }
 
-    public void changeGear(int newGear){
-
-        System.out.println("changeGear method called - The gear is changed to: " + currentGear);
+    public void changeGear(int newGear) {
+        if (newGear >= 1 && newGear <= gears) {
+            currentGear = newGear;
+            System.out.println("Gear changed to: " + currentGear);
+        } else {
+            System.out.println("Invalid gear value");
+        }
     }
 
-    public void changeSpeed(int newSpeed, int newDirection){
-
-        System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
+    public void changeSpeed(int newSpeed, int newDirection) {
+        move(newSpeed, newDirection);
+        System.out.println("Speed changed to: " + newSpeed + " and direction changed to: " + newDirection + " degrees");
+    }
+    public int getCurrentGear() {
+        return currentGear;
     }
 }
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,9 +2,21 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        F1 mercedes = new F1("mercedes", false);
-        mercedes.move(40, 0);
+        F1 mercedes = new F1("Mercedes", false);
+        mercedes.changeSpeed(40, 0);
+        System.out.println("Current Speed: " + mercedes.getCurrentSpeed());
+        System.out.println("Current Gear: " + mercedes.getCurrentGear());
+
         mercedes.accelerate(20);
+        System.out.println("Accelerating...");
+
+        System.out.println("New Speed: " + mercedes.getCurrentSpeed());
+        System.out.println("New Gear: " + mercedes.getCurrentGear());
+
         mercedes.accelerate(-60);
+        System.out.println("Decelerating...");
+
+        System.out.println("New Speed: " + mercedes.getCurrentSpeed());
+        System.out.println("New Gear: " + mercedes.getCurrentGear());
     }
 }
